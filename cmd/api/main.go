786df62c220f1d1efa85013afc375980ee1623fa
Arturo Guerra <ar2roguerra@gmail.com>
2021-01-10 00:31:18 -0600

    Added github for the project api

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/arturoguerra/arturonet-api/internal/emailmanager"
+	"github.com/arturoguerra/arturonet-api/internal/githubmanager"
 	"github.com/arturoguerra/arturonet-api/internal/recaptcha"
 	"github.com/arturoguerra/arturonet-api/internal/router"
 	logging "github.com/arturoguerra/go-logging"
@@ -11,7 +14,7 @@ import (
 func main() {
 	logger := logging.New()
 
-	r, err := router.NewDefault(logger)
+	r, rconfig, err := router.NewDefault(logger)
 	if err != nil {
 		logger.Fatal(err)
 	}
@@ -21,12 +24,25 @@ func main() {
 		logger.Fatal(err)
 	}
 
-	emailGroup := r.Group("/api/email", middleware.Logger())
+	apiGroup := r.Group("/api", middleware.Logger())
+
+	emailGroup := apiGroup.Group("/email")
 	emailManager, err := emailmanager.NewDefault(logger, rcptcha)
 	if err != nil {
 		logger.Fatal(err)
 	}
 
 	emailManager.Register(emailGroup)
+
+	githubGroup := apiGroup.Group("/github")
+	githubManager, err := githubmanager.NewDefault(logger)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	githubManager.Register(githubGroup)
+
+	logger.Infof("Running on %s:%s", rconfig.Host, rconfig.Port)
+	r.Logger.Fatal(r.Start(fmt.Sprintf("%s:%s", rconfig.Host, rconfig.Port)))
 
 }
